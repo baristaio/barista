40ac14924e4c048f99cedb2ff882f87703ca1efe
Vadim Ri <vadim@contracts-square.com>
2018-05-17 16:56:52 +0300

    deploy to CF

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 ### Docker builder
-FROM alpine/git as base_clone
-WORKDIR /opt
-RUN git clone https://f892d5a601e944749f4f3d88482bb96a5d2f8b01@github.com/baristaio/espresso.git
 
-
-
-FROM node:8-alpine as builder
+FROM node:8-alpine
 
 MAINTAINER Vadim Ri <vadim@barista.io>
 
@@ -14,7 +9,7 @@ WORKDIR /app
 RUN chown -R node:node /app
 
 USER node
-COPY --from=base_clone /opt/espresso /opt
+
 # Install app dependencies
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
 # where available (npm@5+)
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@
 
 import 'babel-polyfill';
 
-const espresso = require('espresso');
+const espresso = require('@baristaio/espresso');
 
 import { routes } from './routes';
 
+const PORT = process.env.PORT || 8080;
+console.log('Port =>', PORT);
 
 const localServiceDescriptor = {
     barista: '',
-    port: 3000,
+    port: PORT,
     name: 'My Super Service',
     description: 'the test service',
     routes: routes,
@@ -19,9 +21,9 @@ const localServiceDescriptor = {
 const service = espresso.getService(localServiceDescriptor);
 const stop = service.start();
 
-// stop the service after 1 minute
-setTimeout( ()=> {
-    service.stop('Time expired');
-}, 60* 1000*10);
+// // stop the service after 1 minute
+// setTimeout( ()=> {
+//     service.stop('Time expired');
+// }, 60* 1000*10);
 
 
